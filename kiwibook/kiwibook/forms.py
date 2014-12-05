ae51472cd31d3a6820c5b2cf76decd7c1f647816
pegah1 <ptorkamandi@gmail.com>
2014-12-05 19:59:37 +0330

    clean git :D

--- kiwibook/kiwibook/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.core.exceptions import NON_FIELD_ERRORS
-from django.forms import ModelForm
-import mydatabase.models
-
-class BookForm(ModelForm):
-    class Meta:
-        model=mydatabase.models.Book
-        fields=['id_book' ,'name','author','edition','field','price','rate']
--- search/mydatabase/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from mydatabase.models import Users, Book
-
-
-
-admin.site.register(Book)
-admin.site.register(Users)
-
