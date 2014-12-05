9cb1851d7efc16a83a0a19606ccc716aa08c3cbe
h-alg <top_hoda@yahoo.com>
2014-12-05 18:42:43 +0330

    new project
    esmesh hoda s

--- hoda/hoda/urls.py
+++ hoda/hoda/urls.py
@@ -9,6 +9,17 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     (r'^sefaresh/$', sefaresh),
      (r'^login/$', login),
+     (r'^kerase/$', kerase),
+    (r'^sabtenam/$', sabtenam),
+    (r'^log/$', log),
+    #(r'^aa/$', aa),
+    (r'^aa/(?P<username>.+)/$', aa),
+    (r'^book/(?P<username>.+)/(?P<group>.+)/(?P<page>.+)$', book),
+    (r'^proffer/(?P<username>.+)$', proffer),
+    (r'^infbook/(?P<id_book>.+)$', inf_book),
+
+    ###pegah
+    (r'^search/$', search),
 
     url(r'^admin/', include(admin.site.urls)),
 )
